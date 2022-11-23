b6efc4a135bed86393927048e0c2fccd5fa265b0
Oversaki <shyna@onet.pl>
2022-11-23 18:46:46 +0100

    Poprawki

--- T13 Biblioteki Języków Skryptowych/sketch.js
+++ T13 Biblioteki Języków Skryptowych/sketch.js
@@ -1,21 +1,19 @@
 function setup() {
     createCanvas(1350, 650);
+    textSize(100);
+    strokeWeight(8);
 }
 
 function draw() {
-    if (mouseIsPressed) {
-        erase();
-    } else {
-        fill(202, 255, 183);
-    }
-    ellipse(mouseX, mouseY, 100, 100);
-    fill(238, 255, 183)
-    circle(mouseX, mouseY, 80, 80);
-    fill(255, 236, 183)
-    circle(mouseX, mouseY, 60, 60);
-    fill(255, 200, 183)
-    circle(mouseX, mouseY, 40, 40);
-    fill(255, 183, 202)
-    circle(mouseX, mouseY, 20, 20);
-    fill(255, 183, 238)
+    let x = mouseX;
+    let y = mouseY;
+    let ix = width - mouseX;
+    let iy = height - mouseY;
+    background(196, 247, 230);
+    fill(150, 212, 192);
+    ellipse(x, height/2, y, y);
+    fill(100, 164, 144);
+    ellipse(ix, height/2, iy, iy);
+    text(key, 675, 325);
+
 }
